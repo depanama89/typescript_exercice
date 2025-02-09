b05f89df1b755e4cafdd0c4ad175e0f72829b7f2
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-02-09 17:17:35 +0100

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+// class Car {
+//   public make: string;
+//   public model: string;
+//   public roue: number;
+//   private year: number;
+//   constructor(make: string, roue: number, model: string, year: number) {
+//     this.make = make;
+//     this.model = model;
+//     this.roue = roue;
+//     this.year = year;
+//   }
+//   public getCarInfo(): string {
+//     return `${this.make} ${this.roue} ${this.model} ${this.year}`;
+//   }
+// }
+// const myCar = new Car("toyota", 4, "corolla", 2020);
+// console.log(myCar.getCarInfo());
+let nom = "patrick";
+console.log(nom);
